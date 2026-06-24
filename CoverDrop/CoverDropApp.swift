ca08e78526181d50ada068f3306acb77c766565b
Matt12377 <think_matt@163.com>
2026-06-24 18:39:04 +0800

    Initial Commit

---
+++ CoverDrop/CoverDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverDropApp.swift
+//  CoverDrop
+//
+//  Created by YiHe on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
